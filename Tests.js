efbb2a6222c3a7a254383a502eff432f90121515
Oliver <olivercreativeworks@gmail.com>
2024-05-24 12:27:10 -0400

    Simplify tests

--- Tests.js
+++ Tests.js
@@ -3,72 +3,88 @@
  */
 function testCalendarVacancies(fn = findVacancies){
   GASTEST.describe(fn.name, () => {
-    GASTEST.test('Vacancies should not overlap with existing events', () => {
-      for(let i = 0; i < 1000; i++){
-        const events = Array.from({length: getRandomInt(10)}, _ => {
-          const startHour = getRandomInt(12)
-          const rand = getRandomInt(12)
-          const endHour = startHour + rand
-          if(startHour > endHour){
-            Logger.log(startHour)
-            Logger.log(endHour)
-            Logger.log(rand)
-            throw new Error("end is less than start")
-          }
-          return makeEvent({hour:startHour, minute:0}, {hour:endHour, minute:0})
+    const firstEvent = makeEvent({hour:9}, {hour:9})
+    const lastEvent = makeEvent({hour:17}, {hour:17})
+    const midDayEvent = makeEvent({hour: getRandomIntBetween(9, 12)}, {hour:getRandomIntBetween(13, 17)})
+    const schedule = [firstEvent, lastEvent, midDayEvent]
+
+    const vacancies = fn(schedule)
+
+    GASTEST.describe("Vacancies should not overlap other events", () => {
+      /** @param {Date} time @param {Event} event */
+      const isDuringEvent = (time, event) => event.startTime < time && time < event.endTime
+
+      GASTEST.test("Vacancies should not start during existing events", () => {
+        const validVacancies = vacancies.filter(vacancy => {
+          return schedule.every(event => !isDuringEvent(vacancy.startTime, event))
         })
-        const vacancies = fn(events)
-        console.log((events.toSorted(sortFromEarliestToLatest)))
-        console.log((vacancies))
-        const overlaps = vacancies.map(vacancy => [vacancy, events.find(event => eventsOverlap(vacancy, event))])
-          .filter(overlaps => overlaps[1])
-          .map(([e1, e2]) => ({vacancy: {start: e1.startTime.getHours(), end:e1.endTime.getHours()}, event: {start: e2.startTime.getHours(), end:e2.endTime.getHours()}}))
-        console.log(overlaps)
-        GASTEST.expect(overlaps).toEqual([])
-      }
+        GASTEST.expectEquality(validVacancies, vacancies)
+      })
+
+      GASTEST.test("Vacancies should not end during existing events", () => {
+        const validVacancies = vacancies.filter(vacancy => schedule.every(event => !isDuringEvent(vacancy.endTime, event)))
+        GASTEST.expectEquality(validVacancies, vacancies)
+      })
+    })
+
+    GASTEST.test("Vacancies should occur after the earliest event of the day", () => {
+      const validVacancies = vacancies.filter(vacancy => firstEvent.endTime <= vacancy.startTime)
+      GASTEST.expectEquality(validVacancies, vacancies)
+    })
+
+    GASTEST.test("Vacancies should occur before the latest event of the day", () => {
+      const validVacancies = vacancies.filter(vacancy => vacancy.endTime <= lastEvent.startTime)
+      GASTEST.expectEquality(validVacancies, vacancies)
+    })
+
+    GASTEST.test("Vacancy start times should be before end times", () => {
+      const validVacancies = vacancies.filter(vacancy => vacancy.startTime < vacancy.endTime)
+      GASTEST.expectEquality(validVacancies, vacancies)
+    })
+
+    GASTEST.test("There should be at least one vacancy", () => {
+      const firstEvent = makeEvent({hour:9}, {hour:9})
+      const lastEvent = makeEvent({hour:17}, {hour:17})
+      const schedule = [firstEvent, lastEvent]
+      const vacancies = fn(schedule)
+      GASTEST.expectEquality(vacancies.length > 0, true)
+    })
+
+    GASTEST.test("There should be no vacancies", () => {
+      const firstEvent = makeEvent({hour:9}, {hour:9})
+      const lastEvent = makeEvent({hour:17}, {hour:17})
+      const allDayEvent = makeEvent({hour:firstEvent.endTime.getHours()}, {hour:lastEvent.startTime.getHours()})
+      const schedule = [firstEvent, lastEvent, allDayEvent]
+      const vacancies = fn(schedule)
+      GASTEST.expectEquality(vacancies, [])
     })
   })
 }
 
-/**
- * @param {Event} eventA
- * @param {Event} eventB
- */
-function eventsOverlap(eventA, eventB){
-  return isBetweenExclusive(eventA.startTime , eventB.startTime, eventB.endTime) ||
-  isBetweenExclusive(eventA.endTime , eventB.startTime, eventB.endTime) ||
-  isBetweenExclusive(eventB.startTime, eventA.startTime, eventA.endTime) ||
-  isBetweenExclusive(eventB.endTime, eventA.startTime, eventA.endTime)
+function getRandomIntBetween(startExclusive, endExclusive){
+  return startExclusive + getRandomInt(endExclusive - startExclusive - 1)
+
+  /**
+   * @param {number} max
+   */
+  function getRandomInt(max){
+    return Math.ceil(Math.random() * max)
+  }
 }
 
-/**
- * @param {Date} dateToTest
- * @param {Date} startDate
- * @param {Date} endDate
- */
-function isBetweenExclusive(dateToTest, startDate, endDate){
-  return startDate < dateToTest && dateToTest < endDate
-}
 
 /**
- * @param {number} max
- */
-function getRandomInt(max){
-  return Math.ceil(Math.random() * max)
-}
-
-/**
- * @param {{hour:number, minute:number}} startTime
- * @param {{hour:number, minute:number}} endTime
+ * @param {{hour:number, minute?:number}} startTime
+ * @param {{hour:number, minute?:number}} endTime
  * @return {Event}
  */
 function makeEvent(startTime, endTime){
   return {startTime:makeTime(startTime), endTime:makeTime(endTime)}
-}
 
-/**
- * @param {{hour:number, minute:number}} time
- */
-function makeTime(time){
-  return new Date(new Date().setHours(time.hour, time.minute))
+  /**
+   * @param {{hour:number, minute?:number}} time
+   */
+  function makeTime(time){
+    return new Date(new Date().setHours(time.hour, time.minute || 0))
+  }
 }
